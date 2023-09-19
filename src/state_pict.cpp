d58bdfb434f239ba8df05339cb23a82d891941cd
JimEsterby <jdesterby@gmail.com>
2023-09-19 16:30:11 -0400

    KAN-13 Move component
    Implemented moving and resizing for the state component.

--- src/state_pict.cpp
+++ src/state_pict.cpp
@@ -4,7 +4,16 @@
 #include <FL/fl_draw.H>
 
 StatePict::StatePict(int x, int y, int w, int h, const char* name)
-: Fl_Box(x, y, w, h, name)
+: Fl_Box(x, y, w < minimum_size? minimum_size : w, h < minimum_size? minimum_size : h, name)
+{
+    box(FL_ROUNDED_BOX);
+    align(Fl_Align(FL_ALIGN_TOP|FL_ALIGN_INSIDE));
+    color((Fl_Color)215);  // light yellow
+    labelsize(12);
+}
+
+StatePict::StatePict(int x, int y, const char* name)
+: Fl_Box(x, y, minimum_size, minimum_size, name)
 {
     box(FL_ROUNDED_BOX);
     align(Fl_Align(FL_ALIGN_TOP|FL_ALIGN_INSIDE));
@@ -15,13 +24,25 @@ StatePict::StatePict(int x, int y, int w, int h, const char* name)
 int StatePict::handle(int event)
 {
     static int offset[2] = { 0, 0 };
+    static int rect[4] = { 0, 0, 0, 0 };
+    static int loc = center;
+    int ev_x;
+    int ev_y;
     int result = Fl_Box::handle(event);
+
+    ev_x = Fl::event_x();
+    ev_y = Fl::event_y();
 
     switch (event)
     {
     case FL_PUSH:
-        offset[0] = x() - Fl::event_x();
-        offset[0] = y() - Fl::event_y();
+        loc = mouse_loc();
+        offset[0] = x() - ev_x;
+        offset[0] = y() - ev_y;
+        rect[0] = x();
+        rect[1] = y();
+        rect[2] = x() + w();
+        rect[3] = y() + h();
         result = 1;
         break;
 
@@ -30,7 +51,97 @@ int StatePict::handle(int event)
         break;
 
     case FL_DRAG:
-        position(offset[0] + Fl::event_x(), offset[1] + Fl::event_y());
+        // This is code necessary to handling all of the resizing possibilities
+        // while maintaining a minimum dimensions for the component
+        switch (loc)
+        {
+        case top:
+            if (rect[3] - ev_y >= minimum_size)
+            {
+                resize(rect[0], ev_y, rect[2] - rect[0], rect[3] - ev_y);
+            }
+            break;
+        case bottom:
+            if (ev_y - rect[1] >= minimum_size)
+            {
+                resize(rect[0], rect[1], rect[2] - rect[0], ev_y - rect[1]);
+            }
+            break;
+        case left:
+            if (rect[2] - ev_x >= minimum_size)
+            {
+                resize(ev_x, rect[1], rect[2] - ev_x, rect[3] - rect[1]);
+            }
+            break;
+        case right:
+            if (ev_x - rect[0] >= minimum_size)
+            {
+                resize(rect[0], rect[1], ev_x - rect[0], rect[3] - rect[1]);
+            }
+            break;
+        case top_left:
+            if (rect[2] - ev_x >= minimum_size && rect[3] - ev_y >= minimum_size)
+            {
+                resize(ev_x, ev_y, rect[2] - ev_x, rect[3] - ev_y);
+            }
+            else if (rect[2] - ev_x >= minimum_size)
+            {
+                resize(ev_x, ev_y, rect[2] - ev_x, minimum_size);
+            }
+            else if (rect[3] - ev_y >= minimum_size)
+            {
+                resize(ev_x, ev_y, minimum_size, rect[3] - ev_y);
+            }
+            break;
+        case bottom_left:
+            if (rect[2] - ev_x >= minimum_size && ev_y - rect[1] >= minimum_size)
+            {
+                resize(ev_x, rect[1], rect[2] - ev_x, ev_y - rect[1]);
+            }
+            else if (rect[2] - ev_x >= minimum_size)
+            {
+                resize(ev_x, rect[1], rect[2] - ev_x, minimum_size);
+            }
+            else if (ev_y - rect[1] >= minimum_size)
+            {
+                resize(ev_x, rect[1], minimum_size, ev_y - rect[1]);
+            }
+            break;
+        case top_right:
+            if (ev_x - rect[0] >= minimum_size && rect[3] - ev_y >= minimum_size)
+            {
+                resize(rect[0], ev_y, ev_x - rect[0], rect[3] - ev_y);
+            }
+            else if (ev_x - rect[0] >= minimum_size)
+            {
+                resize(rect[0], ev_y, ev_x - rect[0], minimum_size);
+            }
+            else if (rect[3] - ev_y >= minimum_size)
+            {
+                resize(rect[0], ev_y, minimum_size, rect[3] - ev_y);
+            }
+            break;
+        case bottom_right:
+            if (ev_x - rect[0] >= minimum_size && ev_y - rect[1] >= minimum_size)
+            {
+                resize(rect[0], rect[1], ev_x - rect[0], ev_y - rect[1]);
+            }
+            else if (ev_x - rect[0] >= minimum_size)
+            {
+                resize(rect[0], rect[1], ev_x - rect[0], minimum_size);
+            }
+            else if (ev_y - rect[1] >= minimum_size)
+            {
+                resize(rect[0], rect[1], minimum_size, ev_y - rect[1]);
+            }
+            break;
+
+        // Not resizing, just moving
+        case center:  // move
+            position(offset[0] + Fl::event_x(), offset[1] + Fl::event_y());
+            break;
+        }
+
         ((Fl_Window*)parent())->redraw();
         result = 1;
     }
@@ -43,4 +154,58 @@ void StatePict::draw()
     Fl_Box::draw();
 
     fl_line(x(), y() + fl_height(), x() + w(), y() + fl_height());
+}
+
+// Determines location of mouse within the state picture
+int StatePict::mouse_loc() const
+{
+    int result = center;
+
+    // Mouse coordinates within the StatePict parent window
+    int m_x;
+    int m_y;
+
+    m_x = Fl::event_x();
+    m_y = Fl::event_y();
+
+    if (m_x < x() + border_width)  // Close to the left
+    {
+        if (m_y < y() + border_width)  // Close to the top
+        {
+            result = top_left;
+        }
+        else if (m_y > x() + h() - border_width)  // Close to bottom
+        {
+            result = bottom_left;
+        }
+        else  // (Center) left
+        {
+            result = left;
+        }
+    }
+    else if (m_x > x() + w() - border_width)  // Close to the right
+    {
+        if (m_y < y() + border_width)  // Close to the top
+        {
+            result = top_right;
+        }
+        else if (m_y > x() + h() - border_width)  // Close to the bottom
+        {
+            result = bottom_right;
+        }
+        else  // (Center) right
+        {
+            result = right;
+        }
+    }
+    else if (m_y < y() + border_width)
+    {
+        result = top;
+    }
+    else if (m_y > y() + h() - border_width)
+    {
+        result = bottom;
+    }
+
+    return result;
 }
--- src/state_pict.h
+++ src/state_pict.h
@@ -3,14 +3,24 @@
 
 #include <FL/Fl_Box.H>
 
+// GUI class represents a state picture
 class StatePict : public Fl_Box
 {
 public:
     StatePict(int x, int y, int w, int h, const char* name = 0);
+    StatePict(int x, int y, const char* name = 0);
 
 protected:
     int handle(int event);
     void draw();
+    int mouse_loc() const;
+
+    enum border{ top_left,    top,    top_right,
+                 left,        center, right,
+                 bottom_left, bottom, bottom_right };
+
+    static const int border_width = 15;  // 15 pixels
+    static const int minimum_size = 45;
 
 private:
 
