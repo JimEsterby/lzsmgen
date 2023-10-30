4cf1d42bf7d8d2fa8092ad62b957705b6c891c01
JimEsterby <jdesterby@gmail.com>
2023-10-29 21:32:14 -0400

    Added unit tests for KAN-10 and KAN-19.

--- test/test_model.cpp
+++ test/test_model.cpp
@@ -29,14 +29,43 @@ TEST(Test_Model, failed_script002)
     CHECK_TEXT(result == true, "With no script, should return true.");
 }
 
-#if 0
-// test mocking
-TEST(Test_Model, failed_script003)
+// open_file method
+// successful open of demo
+TEST(Test_Model, opensuccess001)
 {
-    mock().expectOneCall("fake_function");
-
     Model* m = new Model;
-    mock().checkExpectations();
+    m->open_file("..\\demos\\thermostat.json");
+    STRCMP_EQUAL("..\\demos\\thermostat", m->module_name());
     delete m;
 }
-#endif
+
+// open_file method
+// NULL file name
+TEST(Test_Model, openfailure001)
+{
+    Model* m = new Model;
+    bool result = m->open_file(0);
+    delete m;
+    CHECK_TEXT(result == false, "Needs to fail gracefully");
+}
+
+// open_file method
+// bogus file name
+TEST(Test_Model, openfailure002)
+{
+    FAIL("TODO: This is not working, yet.");
+    Model* m = new Model;
+    bool result = m->open_file("thefiledoenstexist.json");
+    delete m;
+    CHECK_TEXT(result == false, "Needs to fail gracefully");
+}
+
+// check generation of module base name
+TEST(Test_Model, basename001)
+{
+    Model* m = new Model;
+    m->file_name("test_file.json");
+    STRCMP_EQUAL("test_file", m->module_name());
+    delete m;
+}
+
